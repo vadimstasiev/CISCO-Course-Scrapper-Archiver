3606009e9057453f552640bf6a5dc6011edbfb6e
vadimstasiev <vadim444@hotmail.com>
2021-01-28 13:27:08 +0000

    implemented email fill, working on password fill

--- pageScraper.js
+++ pageScraper.js
@@ -1,21 +1,54 @@
+const { config } = require('./netacad-config');
+
 const scraperObject = {
-    url: 'http://books.toscrape.com',
+    url: 'https://www.netacad.com/portal/saml_login',
     async scraper(browser){
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
         // Navigate to the selected page
-        await page.goto(this.url);
-        // Wait for the required DOM to be rendered
-        await page.waitForSelector('.page_inner');
-        // Get the link to all the required books
-        let urls = await page.$$eval('section ol > li', links => {
-            // Make sure the book to be scraped is in stock
-            links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
-            // Extract the links from the data
-            links = links.map(el => el.querySelector('h3 > a').href)
-            return links;
-        });
-        console.log(urls);
+        await page.goto(this.url, {waitUntil: 'networkidle2'});
+
+        // Email input
+        let emailInputXPath = '//*[@id="email"]';
+        let nextButtonXPath = '//*[@id="btn"]';
+        await page.waitForXPath(emailInputXPath);
+        const [emailInputHandle] = await page.$x(emailInputXPath);
+        const [nextButtonHandle] = await page.$x(nextButtonXPath);
+
+        await page.evaluate(async (inputEl, nextButtonEl, email) => {
+            inputEl.value = email;
+            nextButtonEl.disabled = false;
+            setTimeout(()=>{
+                console.log('waiting');
+            }, 2000)
+            // nextButtonEl.click();
+        }, emailInputHandle, nextButtonHandle, config.creds.email);
+
+        await Promise.all([
+            nextButtonHandle.click(),
+            page.waitForNavigation({ waitUntil: 'networkidle0' }),
+        ])
+
+
+        // Password Input
+        let passwordInputXPath = '//*[@id="password"]';
+        let signinButtonXPath = '//*[@id="kc-login"]';
+        await page.waitForXPath(emailInputXPath);
+        const [passwordInputHandle] = await page.$x(passwordInputXPath);
+        const [signinButtonHandle] = await page.$x(signinButtonXPath);
+        console.log(passwordInputHandle)
+
+        await page.evaluate(async (inputEl, nextButtonEl, password) => {
+            inputEl.value = password;
+            nextButtonEl.disabled = false;
+            setTimeout(()=>{
+                console.log('waiting');
+            }, 2000)
+            console.log(nextButtonEl)
+            nextButtonEl.click();
+        }, passwordInputHandle, signinButtonHandle, config.creds.password);
+
+        console.log("hit the bottom!");
     }
 }
 
