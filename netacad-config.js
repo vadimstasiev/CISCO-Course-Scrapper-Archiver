ab615b4d84a20c8325c730bdf4ad387d40e1d549
AName* name("Vadim") <vadim444@hotmail.com>
2021-02-04 20:34:18 +0000

    Create netacad-config.js

---
+++ netacad-config.js
@@ -0,0 +1,10 @@
+const config = {
+    creds: {
+        email: 'email@email.com',
+        password: 'password'
+    },
+    course_button_url: 'https://8859338.netacad.com/courses/974157/assignments/18588465?module_item_id=65215495',
+    course_url: 'https://contenthub.netacad.com/itn?lng=en-US'
+};
+
+module.exports = { config };
